341dc3f827aa1c72a0fc76dfe4684a41f2d211ca
Abhishek Kumar Pal <abhishekpaul28hg@gmail.com>
2024-03-27 14:21:51 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+// const slides = document.querySelectorAll('.slide');
+// let index = 0;
+
+// function prevSlide(){
+//     slides[index].classList.remove('active');
+//     index--;
+
+//     if(index < 0)
+//         index = slides.length -1;
+
+//     slides[index].classList.add('active');      
+// }
+
+// document.querySelector('.prev').addEventListener('click', e => {
+//     prevSlide();
+// });
+
+// function nextSlide(){
+//     slides[index].classList.remove('active');
+//     index++;
+
+//     if(index > slides.length -1)
+//         index = 0;
+
+//     slides[index].classList.add('active');      
+// }
+
+// document.querySelector('.next').addEventListener('click', e => {
+//     nextSlide();
+// });
